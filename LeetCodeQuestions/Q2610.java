45ab7320fbae68ea43335e680d832c5ef5525bfb
Ishan3450 <jaganiishan@gmail.com>
2024-01-02 19:45:14 +0530

    Added 2 Jan 2024 Leetcode POTD

---
+++ LeetCodeQuestions/Q2610.java
@@ -0,0 +1,41 @@
+package LeetCodeQuestions;
+
+public class Q2610 {
+    public List<List<Integer>> findMatrix(int[] nums) {
+        int minSize = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            minSize = Math.max(nums[i], minSize);
+        }
+
+        int[] occr = new int[minSize];
+
+        for (int i = 0; i < nums.length; i++) {
+            occr[nums[i] - 1]++;
+        }
+
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < occr.length; i++) {
+            int row = 0;
+            while (occr[i] > 0) {
+                if (map.containsKey(row)) {
+                    map.get(row).add(i + 1);
+                } else {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(i + 1);
+                    map.put(row, list);
+                }
+                row++;
+                occr[i]--;
+            }
+        }
+
+        List<List<Integer>> matrix = new ArrayList<>();
+        for (List<Integer> list : map.values()) {
+            matrix.add(list);
+        }
+
+        return matrix;
+    }
+}
