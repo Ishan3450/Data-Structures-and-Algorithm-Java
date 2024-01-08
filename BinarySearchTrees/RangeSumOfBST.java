4f2873dce10fa3c05a2afda38e1f9aababcb85a9
Ishan3450 <jaganiishan@gmail.com>
2024-01-08 15:14:29 +0530

    Added 8 Jan, 2024 POTD of LeetCode and GFG

--- BinarySearchTrees/RangeSumOfBST.java
+++ BinarySearchTrees/RangeSumOfBST.java
@@ -3,18 +3,24 @@ package BinarySearchTrees;
 public class RangeSumOfBST {
     public int rangeSumBST(TreeNode root, int low, int high) {
         // base condition
-        if (root == null) {
+        if(root == null){
             return 0;
         }
 
         // function body
-        if (root.val >= low && root.val <= high) {
+        if(root.val >= low && root.val <= high){
             return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
-        } else if (root.val < low) {
-            return rangeSumBST(root.right, low, high);
-        } else if (root.val > high) {
-            return rangeSumBST(root.left, low, high);
         }
-        return -1;
+
+        if(root.val < low){
+            return rangeSumBST(root.right, low, high);
+        }
+
+        // root.val > high
+        return rangeSumBST(root.left, low, high);
     }
 }
+
+// if node in range then do both left and right calls
+// if node.val < low then do only right call
+// if node.val > high then do only left call
